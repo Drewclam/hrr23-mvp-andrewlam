684336fbbe52314c831b5a4e8360e5915dbadef2
Drewclam <andrew.c.lam94@gmail.com>
2017-05-01 15:08:44 -0400

    Make https request to riotapi to retrieve challenger league data

--- request-handler.js
+++ request-handler.js
@@ -1,7 +1,22 @@
+var https = require('https');
+var requestParams = {
+  url: 'https://na.api.riotgames.com/api/lol/NA/v2.5/league/challenger',
+  type: 'type=RANKED_SOLO_5x5',
+  apiKey: 'api_key=RGAPI-54200fed-7052-4fc0-be83-08ee2eb6a855'
+};
+
 var methods = {
 
   get: function(req, res) {
-    res.send('handle get request');
+    var body = '';
+    https.get(requestParams.url + '?' + requestParams.type + '&' + requestParams.apiKey, function(result) {
+      result.on('data', function(chunk) {
+        body += chunk;
+      });
+      result.on('end', function() {
+        res.end(body);
+      });
+    });
   }
 
 };
