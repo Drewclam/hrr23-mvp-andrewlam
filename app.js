0067785abe32babee19a611f843a5d2b60b5ba08
Drewclam <andrew.c.lam94@gmail.com>
2017-05-01 14:37:20 -0400

    Add get route to index

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var app = express();
+var requestHandler = require('./request-handler.js');
 
-var port = 3030;
+var port = 8080;
+
+// routes
+// console.log(requestHandler);
+app.get('/', requestHandler.methods.get);
+
+
 
 app.listen(port);
 console.log('app.js listening on port:' + port);
---
+++ request-handler.js
@@ -0,0 +1,18 @@
+var methods = {
+
+  get: function(req, res) {
+    res.send('handle get request');
+  }
+
+};
+
+
+
+
+
+
+
+
+
+
+module.exports.methods = methods;
