eaaed58e83b08128c3c322de4e1a779bfd735711
Drewclam <andrew.c.lam94@gmail.com>
2017-05-01 14:52:43 -0400

    Add connection to mongo database

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var app = express();
 var requestHandler = require('./request-handler.js');
+var mongoose = require('mongoose');
+// make sure mongod is running in shell first!
+mongoose.connect('mongodb://localhost/mvpdb');
+var mvpConnection = mongoose.connection;
+mvpConnection.on('error', console.error.bind(console, 'Error connecting to mongodb'));
+mvpConnection.once('open', function() {
+  console.log('Successfully connected to mongodb');
+});
 
 var port = 8080;
 
 // routes
-// console.log(requestHandler);
 app.get('/', requestHandler.methods.get);
 
 
