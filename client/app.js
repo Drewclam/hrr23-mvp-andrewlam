3470164693cce9fc700e9d95031d6c638edda110
Drewclam <andrew.c.lam94@gmail.com>
2017-05-01 18:27:57 -0400

    Add service to make get request for tier data in frontend

--- client/app.js
+++ client/app.js
@@ -1,7 +1,12 @@
 angular.module('mvp')
 
+.controller('appCtrl', function($scope, leagueData) {
+  console.log('inside appCtrl ', $scope);
+  $scope.onClick = leagueData.getTierData;
+})
+
 .directive('app', function() {
   return {
     templateUrl: './get.html'
   }
-});
+})
---
+++ services/leagueData.js
@@ -0,0 +1,30 @@
+angular.module('mvp')
+
+.service('leagueData', function($http) {
+
+  var requestParams = {
+    url: 'https://na.api.riotgames.com/api/lol/NA/v2.5/league/challenger',
+    type: 'type=RANKED_SOLO_5x5',
+    apiKey: 'api_key=RGAPI-54200fed-7052-4fc0-be83-08ee2eb6a855'
+  };
+
+  this.getTierData = function(callback) {
+    $http.get(requestParams.url + '?' + requestParams.type + '&' + requestParams.apiKey)
+    .then(function success(response) {
+      console.log('success retrieving data');
+      // parse tier data
+      var parsed = {};
+      response.data.entries.forEach(function(entry) {
+        var player = {};
+        player.leaguePoints = entry.leaguePoints;
+        player.wins = entry.wins;
+        player.losses = entry.losses;
+        parsed[entry.playerOrTeamName] = player;
+      });
+      //
+      callback(parsed);
+    }, function error(response) {
+      console.log('error retrieving data');
+    });
+  }
+})
